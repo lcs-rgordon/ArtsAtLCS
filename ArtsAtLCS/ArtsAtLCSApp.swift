215ecd9e5c5038f91a04dd319ffa679a0b8d3c0f
Russell Gordon <rgordon@lcs.on.ca>
2020-10-05 16:53:26 -0400

    Initial Commit

---
+++ ArtsAtLCS/ArtsAtLCSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtsAtLCSApp.swift
+//  ArtsAtLCS
+//
+//  Created by Russell Gordon on 2020-10-05.
+//
+
+import SwiftUI
+
+@main
+struct ArtsAtLCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
