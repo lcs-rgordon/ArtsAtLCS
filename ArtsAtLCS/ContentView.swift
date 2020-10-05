26843a9a55dda6d8be9f59e579ea42cd8d57e97c
Russell Gordon <rgordon@lcs.on.ca>
2020-10-05 17:14:40 -0400

    Got the banner image placed

--- ArtsAtLCS/ContentView.swift
+++ ArtsAtLCS/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Image("banner")
+                .padding(.top, 40)
+            
+            Spacer()
+        }
+        .background(Color.black)
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
