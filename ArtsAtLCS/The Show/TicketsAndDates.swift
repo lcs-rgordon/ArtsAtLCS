b5e51959cc69a1b2ec3c544cd1e8b3b8e1e681f8
Russell Gordon <rgordon@lcs.on.ca>
2020-10-05 20:56:21 -0400

    Tickets and dates page partially completed.

--- ArtsAtLCS/The Show/TicketsAndDates.swift
+++ ArtsAtLCS/The Show/TicketsAndDates.swift
@@ -9,12 +9,58 @@ import SwiftUI
 
 struct TicketsAndDates: View {
     var body: some View {
-        Text("Tickets and dates")
+        
+        GeometryReader { geometry in
+            
+            VStack(alignment: .leading,
+                   spacing: 10) {
+                
+                Text("Showings")
+                    .font(.title2)
+                    .padding(.top, 10)
+
+                VStack(alignment: .leading,
+                       spacing: 5) {
+                    Text("Tuesday, November 26 @ 7:30 PM")
+                    Text("Wednesday, November 27 @ 7:30 PM")
+                    // TODO: Add remaining dates.
+
+                }
+                .padding(.horizontal)
+                
+                Text("Tickets")
+                    .font(.title2)
+                    .padding(.top, 10)
+                
+                VStack(alignment: .leading,
+                       spacing: 5) {
+                    Text("Seating may still be available.")
+                    // TODO: Add link about getting tickets
+
+                }
+                .padding(.horizontal)
+
+            }
+            .padding(.horizontal)
+            .frame(minWidth: geometry.size.width,
+                   alignment: .leading)
+
+
+
+        }
+        .navigationBarTitle("Tickets and Dates", displayMode: .inline)
+
     }
 }
 
 struct TicketsAndDates_Previews: PreviewProvider {
     static var previews: some View {
+        
         TicketsAndDates()
+            .environment(\.colorScheme, .dark)
+        
+        TicketsAndDates()
+            .environment(\.colorScheme, .light)
+
     }
 }
